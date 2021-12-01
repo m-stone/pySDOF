56255fa6d6556a4f521348e3a689af8a7ffe2ae3
m-stone <stone.ma@gmail.com>
2021-11-30 19:19:46 -0500

    Update SDOF.py

--- source/SDOF.py
+++ source/SDOF.py
@@ -6,3 +6,18 @@ Created on Mon Nov 29 14:19:59 2021
 """
 
 import numpy as np
+
+class SDOF():
+    def __init__(self, mass, stiffness, damping_ratio):
+        self.m = mass
+        self.k = stiffness
+        self.xi = damping_ratio
+        self.set_damping()
+        
+    def set_damping(self):
+        self.omega = np.sqrt(self.k / self.m)
+        self.c_crit = self.m * self.omega
+        self.c = self.xi * self.c_crit
+        
+    def solve(self, solver='CDM'):
+        pass
